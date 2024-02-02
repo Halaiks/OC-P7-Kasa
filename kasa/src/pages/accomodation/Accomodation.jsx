4f86590bc652a78061b9a38d512b237cf6dfe115
Alexandre GUILLAUME <AlexGuillaume94@gmail.com>
2024-02-02 21:27:47 +0100

    Création du routeur + pages

---
+++ kasa/src/pages/accomodation/Accomodation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Accomodation() {
+    return (
+		<div class="accomodation">
+			<h2>Accomodation</h2>
+		</div>
+	)
+}
---
+++ kasa/src/pages/notFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+	return (
+		<div className='notFound'>
+			<h2>Not found</h2>
+		</div>
+	)
+}
