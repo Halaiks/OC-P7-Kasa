44b5c8b16bcec56c104d0384c4471d06192530a1
Alexandre GUILLAUME <AlexGuillaume94@gmail.com>
2024-02-03 07:37:24 +0100

    Création page d'accueil
    Création de la page d'accueil avec composants banner, cards et gallery

--- kasa/src/pages/home/Home.jsx
+++ kasa/src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './home.scss'
 import Header from '../../components/header/Header'
+import Banner from '../../components/banner/Banner'
+import Gallery from '../../components/gallery/Gallery'
 import Footer from '../../components/footer/Footer'
 
 export default function Home() {
 	return (
 		<div className='home'>
-            <Header />
+			<Header />
+			<Banner />
+			<Gallery />
 			<Footer />
 		</div>
 	)
