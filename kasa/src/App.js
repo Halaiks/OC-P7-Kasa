d6f710c14d0f3ddea81f790b636d43e2d7f5388c
Alexandre GUILLAUME <AlexGuillaume94@gmail.com>
2024-02-03 08:22:04 +0100

    Ajout de commentaires
    Fichiers "App.js" et "Index.js"

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,11 +1,13 @@
+// Imports
 import Home from './pages/home/Home';
 import NotFound from './pages/notFound/NotFound';
 import About from './pages/about/About';
 import Accomodation from './pages/accomodation/Accomodation';
+
+// Import des utilitaires de routage de React
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-
-
+// Configuration des routes de l'application
 const router = createBrowserRouter([
 	{
 		path: "/",
@@ -25,12 +27,15 @@ const router = createBrowserRouter([
 	},
 ]);
 
+// Composant principal de l'application
 function App() {
 	return (
 		<>
+			{/* Fournit le routeur configuré aux composants enfants */}
 			<RouterProvider router={router}/>
 		</>
 	);
 }
 
+// Exporte le composant App pour être utilisé ailleurs dans l'application
 export default App;
--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,8 +1,13 @@
+// Imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Création d'une racine React avec le DOM de l'élément ayant l'ID 'root'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendu du composant App dans la racine avec le mode StrictMode activé
+// StrictMode permet de détecter certaines pratiques potentiellement problématiques dans le code et affiche des avertissements en conséquence.
 root.render(
 	<React.StrictMode>
 		<App  />
